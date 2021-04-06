e4f43c55c98ae31900f34ebf1d87f09201c7ef19
Alexandre LAM <alexandrelam@outlook.com>
2021-04-06 14:16:30 +0200

    initial commit

---
+++ 2048_LAM_TALBI/Cellule.swift
@@ -0,0 +1,54 @@
+//
+//  Cellule.swift
+//  2048_LAM_TALBI
+//
+//  Created by admin on 30/03/2021.
+//
+
+import Foundation
+import UIKit
+
+class GameCell: UICollectionViewCell {
+    var valeur = 0 {
+        didSet{
+            dessineCellule();
+        }
+    }
+    var implique = false;
+    var texte: UILabel! = nil;
+    
+    override init(frame: CGRect){
+        super.init(frame: frame);
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+    }
+    
+    func dessineCellule() {
+        if texte == nil {
+            texte = UILabel(frame: CGRect(x:0, y:0,width:self.bounds.size.width,height:self.bounds.size.height))
+            texte.numberOfLines = 1
+            texte.textAlignment = .center
+            texte.textColor = UIColor.white
+        }
+        switch valeur {
+        case let x where x >= 2 && x <= 16:
+            texte.text = "\(x)"
+            self.backgroundColor = UIColor.lightGray
+        case let x where x >= 32 && x <= 256:
+            texte.text = "\(x)"
+            self.backgroundColor = UIColor.yellow
+        case let x where x >= 512 && x <= 2048:
+            texte.text = "\(x)"
+            self.backgroundColor = UIColor.brown
+        case let x where x > 2048:
+            texte.text = "\(x)"
+            self.backgroundColor = UIColor.red default:
+                texte.text = ""
+                self.backgroundColor = UIColor.darkGray
+                
+        }
+        self.contentView.addSubview(texte) }
+    
+}
---
+++ 2048_LAM_TALBI/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  2048_LAM_TALBI
+//
+//  Created by admin on 30/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    
+    
+    @IBOutlet weak var grille2048: UICollectionView!;
+    @IBOutlet weak var remplir: UIButton!;
+    
+    var nombreLignes = 4;
+    var nombreColonnes = 4;
+    let espacementCellules = 10;
+    var cellules: [[GameCell?]];
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.nombreLignes = 4
+            self.nombreColonnes = 4
+            cellules = [[]]
+            cellules = ([[GameCell?]](repeating: [], count: nombreLignes))
+            for j in 1...nombreLignes {
+                let ligne = [GameCell?](repeating: nil, count: nombreColonnes)
+                cellules[j-1] = ligne
+                
+            }
+        
+            super.init(coder: aDecoder)
+    }
+    
+    init?(coder aDecoder: NSCoder, nombreLignes: Int, nombreColonnes:Int) { self.nombreLignes = nombreLignes
+        self.nombreColonnes = nombreColonnes
+        cellules = [[]]
+        cellules = ([[GameCell?]](repeating: [], count: nombreLignes))
+        for j in 1...nombreLignes {
+            let ligne = [GameCell?](repeating: nil, count: nombreColonnes)
+            cellules[j-1] = ligne
+            
+        }
+    
+        super.init(coder: aDecoder)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        cellules[indexPath.section][indexPath.row]!.dessineCellule()
+        return cellules[indexPath.section][indexPath.row]!
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return nombreLignes
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width:0, height:CGFloat(espacementCellules))
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    
+        return CGSize(width: (grille2048.frame.width - CGFloat(espacementCellules*(nombreColonnes+2))) / CGFloat(nombreColonnes), height: (grille2048.frame.height - CGFloat(espacementCellules*(nombreLignes+2))) / CGFloat(nombreColonnes));
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return nombreLignes
+    }
+    @IBAction func rempli(sender: UIButton) {
+        for i in 0..<nombreLignes {
+            for j in 0..<nombreColonnes {
+                cellules[i][j]!.valeur = Int(pow(2, Double(j+i*j)))
+                
+                                
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        grille2048.delegate = self
+        grille2048.dataSource = self
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: CGFloat(espacementCellules), bottom: 0, right: CGFloat(espacementCellules))
+        layout.minimumLineSpacing = CGFloat(2)
+        grille2048.setCollectionViewLayout(layout, animated: false)
+        for i in 0...3{
+            for j in 0...3{
+                cellules[i][j] = grille2048.dequeueReusableCell(withReuseIdentifier: "cell2048", for:
+                    NSIndexPath(row: i, section: j) as IndexPath) as? GameCell
+            }
+            
+        }
+        grille2048.backgroundColor = UIColor.gray
+        
+    }
+    
+
+
+}
+
