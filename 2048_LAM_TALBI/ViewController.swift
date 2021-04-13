b1910790cb9c7d746b6e9f8d536da975a1ee1f8d
Aurelien627 <aurelien.talbi@etu.imt-lille-douai.fr>
2021-04-13 07:16:31 -0700

    cleanup folder without databse files

--- 2048_LAM_TALBI/ViewController.swift
+++ 2048_LAM_TALBI/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     @IBOutlet weak var grille2048: UICollectionView!;
     @IBOutlet weak var remplir: UIButton!;
     @IBOutlet weak var scoreAffich: UILabel!;
-    @IBOutlet var tableView: UITableView!;
+    @IBOutlet weak var tableView: UITableView!;
     
     
     var score = 0;
@@ -284,8 +284,8 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         super.viewDidLoad()
 
         // var for table view for score
-        tableView.delegate = self
-        tableView.dataSource = self
+        //tableView.delegate = self
+        //tableView.dataSource = self
 
         // test database fetch
         database.child("score").observeSingleEvent(of: .value, with : {snapshot in 
